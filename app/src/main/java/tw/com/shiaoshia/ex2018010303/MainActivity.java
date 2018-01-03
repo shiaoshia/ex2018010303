80c46d7a85dd8db99565da56658a3ab00d502563
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2018-01-03 15:48:20 +0800

    WebView

--- app/src/main/java/tw/com/shiaoshia/ex2018010303/MainActivity.java
+++ app/src/main/java/tw/com/shiaoshia/ex2018010303/MainActivity.java
@@ -2,6 +2,7 @@ package tw.com.shiaoshia.ex2018010303;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -13,7 +14,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         wv = (WebView)findViewById(R.id.webView);
-        wv.setWebViewClient(new WebViewClient());
-        wv.loadUrl("http://tw.yahoo.com");
+        //wv.setWebViewClient(new WebViewClient());
+        wv.setWebChromeClient(new WebChromeClient());
+        wv.getSettings().setJavaScriptEnabled(true);
+        //wv.loadUrl("https://www.google.com.tw/");
+        //wv.loadData("<iframe width=\"640\" height=\"360\" src=\"https://www.youtube.com/embed/jXmPkWDeAcE\" frameborder=\"0\" gesture=\"media\" allow=\"encrypted-media\" allowfullscreen></iframe>","text/html; charset=UTF-8",null);
+        wv.loadData("data","text/html","UTF-8");
     }
 }
